64df91eb042671caf7a16e70b9c8a29cf4cf3487
SakheleMndaweni <sakhele666@gmail.com>
2024-04-14 13:18:22 +0200

    developement

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path("i18n/", include("django.conf.urls.i18n")),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('admin_app.urls')),
